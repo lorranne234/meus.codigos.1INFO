106674cad7a1c11f16b82681eb47d80a9f8872fc
lorranne234 <isabelelorrane08092010@gmail.com>
2026-07-02 07:54:31 -0300

    Atividade da aula 4 do livro

---
+++ usuario_senha.py
@@ -0,0 +1,9 @@
+usuario = input("Digite o nome do seu usuário:")
+senha = input("Digite a sua senha:")
+
+if usuario == "protocopio" and senha == "1234":
+    print  ("Seja bem vindo!")
+elif usuario == "paiva" and senha == "54321":
+    print ("Seja bem vindo!")
+else:
+    print("Usuário e senha não conferem")
