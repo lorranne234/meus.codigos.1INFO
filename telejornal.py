fd128573f055aff090c5145c79cac3b697ef65f7
lorranne234 <isabelelorrane08092010@gmail.com>
2026-07-02 10:39:23 -0300

    entrega da atividade 4 classroom

---
+++ telejornal.py
@@ -0,0 +1,11 @@
+(“””• Bom Dia Nação, apresentado por Zé PINHEIRO e por Ana Carla ARAÚJO
+• Jornal Brasileiro, apresentado por Bill BONNER E CARLA VASCONCELOS”””)
+
+sobrenome = input("Digite o sobrenome do apresentador: ")
+
+if sobrenome == "PINHEIRO e ARAÚJO":
+    print("Telejornal: Bom Dia Nação")
+elif sobrenome == "BONNER e VASCONCELOS":
+    print("Telejornal: Jornal Brasileiro")
+else:
+    print("Apresentador(a) desconhecido(a).")
