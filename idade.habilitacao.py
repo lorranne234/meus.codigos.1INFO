cf62430ffb137bb9c416f27b3eb9a08ea976cf24
lorranne234 <isabelelorrane08092010@gmail.com>
2026-07-02 10:47:22 -0300

    entrega da atividade 4 classroom

---
+++ idade.habilitacao.py
@@ -0,0 +1,7 @@
+idade = int(input("Digite sua idade: " ))
+habilitacao = input ("Você tem habilitação? ")
+
+if idade == 18 and habilitacao == "Sim":
+    print("Você pode dirigir")
+else:
+    print("Você não pode dirigir")
