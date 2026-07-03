a64542716ae9c06eeb7a329bd3cc9afe64e1dc6e
lorranne234 <isabelelorrane08092010@gmail.com>
2026-07-03 08:35:03 -0300

    assunto novo de estrutura de repetição: while e for !

---
+++ while_senha.py
@@ -0,0 +1,7 @@
+senha = int(input("Senha: "))
+while senha != 12345:
+    print ("Tente novamente. ")
+    senha = int(input("Senha: "))
+print ("Acesso liberado! ")
+    #a senha tem que ser igual a: 12345. enquanto o usuário digitar uma senha != 12345, vai imprimir na tela "tente novamente"
+    #vamos supor que essa é uma senha que o usuário criou no gmail e só ele sabe, e ele só consegue acessar se ele digitar essa senha que ele criou "12345"
